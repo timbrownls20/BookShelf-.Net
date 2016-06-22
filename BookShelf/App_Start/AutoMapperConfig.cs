12e38f22772e2f972a1e75637a0c248a80f6c4da
timbrownls20 <timbrownls26@hotmail.com>
2016-06-22 14:01:29 +0100

    include missing files

---
+++ BookShelf/App_Start/AutoMapperConfig.cs
@@ -0,0 +1,21 @@
+﻿using BookShelf.Models;
+using BookShelf.WebServices.Google.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookShelf
+{
+    public static class AutoMapperConfig
+    {
+        public static void RegisterMappings()
+        {
+            AutoMapper.Mapper.CreateMap<GoogleBook, Book>()
+                .ForMember(dest => dest.BookID, opts => opts.Ignore())
+                .ForMember(dest => dest.ISBN, opts => opts.Ignore())
+                .ForMember(dest => dest.Title, opts => opts.MapFrom(src => src.VolumeInfo.Title))
+                .ForMember(dest => dest.Description, opts => opts.MapFrom(src => src.VolumeInfo.Description));
+        }
+    }
+}
