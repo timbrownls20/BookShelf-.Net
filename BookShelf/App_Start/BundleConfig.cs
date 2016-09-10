c451bcce316c618fc47c3f439886cd77b6cade26
timbrownls20 <timbrownls26@hotmail.com>
2016-09-10 18:34:55 +0100

    add handlerbars.js and finish adding authors functionality

--- BookShelf/App_Start/BundleConfig.cs
+++ BookShelf/App_Start/BundleConfig.cs
@@ -23,6 +23,9 @@ namespace BookShelf
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/handlebars").Include(
+                      "~/Scripts/handlebars.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
