7bf5dab7c0fa034c6edad16dd6e0c7e020cf129c
timbrownls20 <timbrownls26@hotmail.com>
2016-11-20 14:29:16 +0000

    single page application - initial work

---
+++ BookShelf.SPA/app/demo.js
@@ -0,0 +1,13 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('Demo', demo);
+
+    function demo() {
+        var vm = this;
+        vm.food = 'pizza';
+    }
+
+})();
---
+++ BookShelf.SPA/app/searchcontroller.js
@@ -0,0 +1,33 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('searchController', searchController);
+
+    //searchController.$inject = ['$location']; 
+
+    function searchController($scope) {
+        /* jshint validthis:true */
+        var vm = this;
+        vm.title = 'searchController';
+
+        activate();
+
+        function activate() { }
+
+        $scope.searchTerm = "";
+        $scope.debugMessage = "Hello World";
+
+        $scope.search = function () {
+            //alert(msg);
+            alert($scope.searchTerm);
+        };
+    }
+})();
+
+//var ngApp = angular.module('myapp', []);
+
+//ngApp.controller('searchController', function ($scope) {
+//    $scope.debugMessage = "Hello World!";
+//});
